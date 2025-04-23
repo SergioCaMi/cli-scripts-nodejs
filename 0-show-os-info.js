8a2f5e278e63fb2ded088277de870c79e24b92c8
SergioCaMi <sergiocami84@gmail.com>
2025-04-23 21:07:23 +0200

    done

--- 0-show-os-info.js
+++ 0-show-os-info.js
@@ -6,5 +6,5 @@
 const os = require('os');
 
 console.log(`Nombre del equipo:${os.hostname()}`);
-console.log(`Versión del sistema operativo: ${os.type()} ${os.release()}`);
+console.log(`Versión del sistema operativo: ${os.version()}`);
 console.log(`Memoria RAM total:${(os.totalmem() / (1024 ** 3)).toFixed(2)} GB`);
